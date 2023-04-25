34bfc8e8d8f7d06e14f9edae5af3a553a9c3114c
Celso Milne <celsomilne@gmail.com>
2023-04-25 16:59:41 +1000

    Created scholar search API, extending serapi

---
+++ gpt_literature_scraper/SERP_api/ScholarSearch.py
@@ -0,0 +1,29 @@
+from gpt_literature_scraper.SERP_api.Languages import Languages
+from serpapi import GoogleScholarSearch
+
+
+class ScholarSearch:
+
+    def __init__(self, api_key):
+        self.api_key = api_key
+
+    def get(self, query, language="en", **params):
+
+        # Check if the language code is valid
+        if language not in Languages.as_dict():
+            raise Exception("Language code not found")
+
+        # TODO: add more parameters
+        search = GoogleScholarSearch({
+            "q": query, 
+            "api_key": self.api_key,
+            "hl": language,
+            **params,
+        })
+
+        # TODO: add error handling
+        
+        # Get the result as a dictionary
+        result = search.get_dict()
+
+        return result
