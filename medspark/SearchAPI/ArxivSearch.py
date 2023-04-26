9985288f496fc34f88ce7e360388aeb8e2c39494
Celso Milne <celsomilne@gmail.com>
2023-04-26 17:10:34 +1000

    Implemented arxiv search

--- medspark/SearchAPI/ArxivSearch.py
+++ medspark/SearchAPI/ArxivSearch.py
@@ -1,12 +1,43 @@
+import warnings
+from medspark.SearchAPI import Result
 from medspark.SearchAPI.Languages import Languages
 from medspark.SearchAPI.SearchBase import SearchBase
 
+import arxiv
+from arxiv import SortCriterion, SortOrder
+
 # Arxiv search class, extends SearchBase
 class ArxivSearch(SearchBase):
+
     def __init__(self, api_key):
         super().__init__(api_key)
 
-    def get(self, query, language="en", **params):
+    def get(self, query, language="en", sort_by=SortCriterion.Relevance, sort_order=SortOrder.Descending, max_results=float('inf'), **params):
+        
         # Check if the language code is valid
         if language not in Languages.as_dict():
             raise Exception("Language code not found")
+        
+        # Raise a warning that language code is not supported for Arxiv searches, if language is not english
+        if language != "en":
+            warnings.warn("Language code not supported for Arxiv searches")
+
+        # Create the arguments
+        args = {
+            "query": query,
+            "max_results": max_results,
+            "sort_by": sort_by,
+            "sort_order": sort_order,
+            **params,
+        }
+
+        # Perform the search
+        search = arxiv.Search(
+            **args
+        )
+
+        # Return the results
+        return [Result.from_arxiv_result(x) for x in search.results()]
+
+
+        
